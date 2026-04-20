6a92346401cea02a39b23544d0bfa687d0847e31
AdmiralBarbarossa <263571455+AdmiralBarbarossa@users.noreply.github.com>
2026-04-20 19:54:13 -0400

    waybar updates for external displays

--- scripts/wlsunset-toggle.sh
+++ scripts/wlsunset-toggle.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 TEMP=3200
 
-if pidof wlsunset > /dev/null; then
-    killall wlsunset
+if pgrep -x wlsunset > /dev/null; then
+    pkill -x wlsunset
     notify-send -t 1500 "Night light OFF"
 else
     wlsunset -t $TEMP -T 6500 -S 00:00 -s 00:01 &
